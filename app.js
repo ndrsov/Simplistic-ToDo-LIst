dd70366d86c82a5e6b755f9831452d91c08f7377
Andres Soria <andres.zov@gmail.com>
2022-10-15 10:54:36 -0500

    Craete function to use right click to remove the specific todo

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ function addTodo(todo) {
     todoEl.addEventListener("click", () =>
       todoEl.classList.toggle("completed")
     );
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+    });
 
     todosUl.appendChild(todoEl);
 
