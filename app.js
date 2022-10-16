d864f7ae16115057ede7ff8ee22542c80420ae66
Andrés Soria <Andres.zov@gmail.com>
2022-10-16 13:59:27 -0500

    Create function to save todos using Local Browser API

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todosUl = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,16 +29,37 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
-    todoEl.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-      todoEl.remove();
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
     });
 
-    todosUl.appendChild(todoEl);
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
